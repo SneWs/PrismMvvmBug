9ec8950881c2f7210cba6075eef392f33a9e96ee
Marcus Grenängen <marcus@grenangen.se>
2020-01-21 21:35:48 +0100

    Initial add

---
+++ PrismHotReloadBug/PrismHotReloadBug/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Prism;
+using Prism.Ioc;
+using PrismHotReloadBug.ViewModels;
+using System;
+
+namespace PrismHotReloadBug
+{
+    public partial class App
+    {
+        public App()
+            : this(null)
+        {
+        }
+
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("Root");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>("Root");
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Global unhandled exception caught: {e.ExceptionObject}");
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+    }
+}
---
+++ PrismHotReloadBug/PrismHotReloadBug/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace PrismHotReloadBug.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _helloText = "Hello, Prism MVVM";
+        public string HelloText
+        {
+            get => _helloText;
+            set => SetProperty(ref _helloText, value);
+        }
+
+        void INavigatedAware.OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        void INavigatedAware.OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+    }
+}
